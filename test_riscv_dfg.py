37c12f6ef22d459f3ddbd524f91046b51d286478
Connor <connorgreenwood317@gmail.com>
2022-09-06 12:29:28 -0400

    fixed test issue

--- test_riscv_dfg.py
+++ test_riscv_dfg.py
@@ -3,7 +3,7 @@ import Data_Flow_Graph as DFG
 
 dfg = DFG.Data_Flow_Graph('gcn3_strip.ll')
 
-rdfg = RDFG.RiscV_dfg(dfg)
+rdfg = RDFG.RiscV_dfg(dfg, False)
 
 rdfg.Remove_WorkNode_Links()
 rdfg.Remove_Empty_Blocks()
